19db77b397f649e11d20cc60ec28ee19f52f8806
Steven Broad <sebroad@yahoo.com>
2013-12-10 17:30:29 -0500

    Adding details to assignments

--- src/cp/lab/Lab10.java
+++ src/cp/lab/Lab10.java
@@ -7,7 +7,51 @@
 package cp.lab;
 
 /**
- *
+ * <p>Lab 11: Mashup II Directions and Knowledge</p>
+ * <p>CPSC 207: Computer Programming</p>
+ * 
+ * <p>Make a GUI program that computes directions from an origin to a 
+ * destination. Also provide some kind of information about the origin and 
+ * destination using the WolframAlpha web service and optionally other web 
+ * services of your choice.</p>
+ * 
+ * <p>Also begin thinking about how to design your final project.</p>
+ * 
+ * <p>Set up your package and get your GUI started.</p>
+ * 
+ * <ol><li>Review the Directions documentation: <br/>
+ * http://sites.saintmarys.edu/~sbroad/javadoc/edu/saintmarys/google/Directions.html<br/>
+ * The method you want to use is Directions.stepByStep() which produces step by 
+ * step directions in HTML format. This information should go into a JEditorPane 
+ * component. The JEditorPane component should be on a JScrollPane.
+ * 
+ * <li>In addition to the directions, your program will use the WolframAlpha web 
+ * service to retrieve information about the Destination. Check out 
+ * http://sites.saintmarys.edu/~sbroad/javadoc/edu/saintmarys/wolfram/WolframAlpha.html</li>
+ * 
+ * <li>Draw a picture of what your GUI will look like.</li>
+ * 
+ * <li>Create a Java Application project. Replace the text in the class file 
+ * that it creates with the text in this file.</li>
+ * 
+ * <li>Add a JFrame form.</li>
+ * 
+ * <li>Layout your GUI.</li>
+ * 
+ * <li>Create event handling methods for the relevant components.</li>
+ * 
+ * <li>Implement those methods to get information from Google Directions and 
+ * WolframAlpha and display that info in the relevant components.</li>
+ * 
+ * <li>Draw a picture of a GUI for a possible final project.</li></ol>
+ * 
+ * For your final project you will choose a GUI program to make and use web 
+ * services of your choice to access it. Please consult your instructor for 
+ * advice, etc.
+ * 
+ * Please browse http://sites.saintmarys.edu/~sbroad/javadoc/index.html?overview-summary.html 
+ * and follow the links to discover other web services that we have not yet used.
+ * 
  * @author sbroad
  */
 public class Lab10 {

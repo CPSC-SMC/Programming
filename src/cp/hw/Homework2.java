e356b388a35a1e16085c4b44594b3c8cadf26e87
javaprofsmc <sbroad@saintmarys.edu>
2013-12-10 13:45:59 -0500

    Initial Skeleton

--- src/cp/hw/Homework2.java
+++ src/cp/hw/Homework2.java
@@ -6,10 +6,39 @@
 
 package cp.hw;
 
+import cp.HomeworkException;
+
 /**
- *
+ * <p>CPSC 207: Computer Programming<br/>
+ * Homework 2: Strings, objects, methods, parameters </p>
+ * 
+ * <p>Complete each of the methods in Homework2 below. Each method describes what
+ * it is supposed to do. Use the <code>return</code> statement to return the
+ * required information.</p>
+ * 
+ * <p>Create a project called Homework2. Copy this file over the contents of
+ * Homework2.java. The main method does not need to be modified.</p>
+ * 
+ * <p>Submit your project in the usual way. See Lab01 for details.</p>
+ * 
  * @author sbroad
  */
 public class Homework2 {
+    /**
+     * Compute the initials for a person with a certain first and last name.
+     * Make sure that you make them upper case. Use either the <code>charAt</code>
+     * method or the <code>substring</code> method.
+     * @param firstName the first name
+     * @param lastName the last name
+     * @return the upper case initials that go with firstName lastName
+     */
+    public static String getInitials(String firstName, String lastName) {
+        throw new HomeworkException();
+    }
+    
+    public static void main(String [] args) {
+        
+    }
+    
     
 }

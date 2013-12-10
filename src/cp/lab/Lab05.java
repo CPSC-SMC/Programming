928d9c7f50cd2794036c030d49de0237a6daf4cd
Steven Broad <sebroad@yahoo.com>
2013-12-10 17:05:17 -0500

    Adding details to assignments

--- src/cp/lab/Lab05.java
+++ src/cp/lab/Lab05.java
@@ -7,9 +7,55 @@
 package cp.lab;
 
 /**
- *
+ * CPSC 207: Computer Programming
+ * Lab 5: Twitter Queries and Loops
+ * 
+ * In this program, the user will enter Twitter queries. The program will print 
+ * the all Tweets that satisfy the search criteria. Each tweet printed should 
+ * include the date and time of the tweet, the tweet text and the number of 
+ * characters in the tweet. The program should ask if the user has more 
+ * searches to request. If so, the program should continue requesting queries 
+ * and usernames. Otherwise, the program should exit after giving a farewell 
+ * message.
+ * 
+ * 1. Create a Lab05 project.
+ * 
+ * 2. You will need to add the twitter4j-core-3.0.3.jar to the libraries for
+ * the project. This is available from the Java Jar files folder in Blackboard.
+ * Add this .jar file to your Project as usual.
+ * 
+ * 3. Download and save 
+ * https://docs.google.com/file/d/0B_i1NxACvw9yMERrMFoyQnBIMTQ/edit?usp=sharing
+ * in the Lab5/src folder. Make sure that it shows up in your project browser in 
+ * NetBeans.
+ * 
+ * 4. You can do all of your work in Lab05.java.
+ * 
+ * 5. This lab involves two loops. The outer loop will continue looping as long 
+ * as the user says "yes" when prompted if they want to continue. Each time 
+ * through the loop, the program will prompt the user for a search, then run the
+ * search and print the results.
+ * 
+ * 6. One loop will be "inside" the other. This inner loop is where the search 
+ * results are printed. The inner loop is the one shown in the example mentioned 
+ * below. Review example #4: http://twitter4j.org/en/code-examples.html
+ * 
+ * 7. To determine if two strings are the same, you should use either the 
+ * equals() method as in section 5.3 or the equalsIgnoreCase() method which is 
+ * very similar.
+ * 
+ * 8. Complete and generate your javadoc.
+ * 
+ * 9. Submit your project.
  * @author sbroad
  */
 public class Lab05 {
     
+    /**
+     *
+     * @param args
+     */
+    public static void main(String [] args) {
+        
+    }
 }

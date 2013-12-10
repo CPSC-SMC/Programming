24c72660ea448795d4d351f7d8f4dc299581a3ba
javaprofsmc <sbroad@saintmarys.edu>
2013-12-10 15:02:01 -0500

    Adding assignment details.

--- src/cp/lab/Lab08.java
+++ src/cp/lab/Lab08.java
@@ -6,10 +6,55 @@
 
 package cp.lab;
 
+import java.awt.Graphics;
+
 /**
- *
+ * <p>Lab 8: Drawing in Java</p>
+ * <p>CPSC 207: Computer Programming</p>
+ * 
+ * <p>If it is Fall semester, make a Java applet in which you draw a turkey and 
+ * add the text Happy Thanksgiving!</p>
+ * 
+ * <p>If it is Spring semester, make a Java applet in which you draw a bunny and
+ * add the text Happy Easter!</p>
+ * 
+ * <ol><li>Choose an image (possibly from a Google search) of a turkey to use as 
+ * a model.</li>
+ * 
+ * <li>Create a new project. Copy this file over the text in Lab08.java.</li>
+ * 
+ * <li>Make sure that the class Lab08 extends JApplet. (Why?)</li>
+ * 
+ * <li>Declare a public void paint(Graphics page) method in the Lab8 class.</li>
+ * 
+ * <li>Implement the code that paints your picture. You may want to use the 
+ * JPolyDraw applet to help you draw polygons. http://sites.saintmarys.edu/~sbroad/jPolyDraw/</li>
+ * 
+ * <li>Publish your applet to your Saint Mary’s web page.</li>
+ * 
+ * <li>Publish your site: Visit http://sites.saintmarys.edu/ and follow 
+ * instructions to publish your page.</li>
+ * 
+ * <li>Look in My Documents for the NetBeansProjects folder. In that folder, 
+ * find Lab08. In Lab08, find the build folder. Copy everything in the build 
+ * folder into H:\WWW\. )</li>
+ * 
+ * <li>Now visit http://sites.saintmarys.edu/~<username>/Lab08.html</li>
+ * 
+ * <li>If the Turkey or Bunny doesn’t show up completely, open notepad and edit 
+ * H:\WWW\Lab08.html.  About two thirds of the way down, you’ll see <code>
+ * <APPLET codebase="classes" code="lab8/Turkey.class" width=350 height=200>
+ * </APPLET></code>. Just change the width and height until you can see the 
+ * whole turkey!</li>
+ * 
+ * <li>Submit your project. Email the instructor a link to your Turkey or Bunny</li></ol>
+ * 
  * @author sbroad
  */
 public class Lab08 {
+    
+    public void paint(Graphics page) {
+        
+    }
     
 }

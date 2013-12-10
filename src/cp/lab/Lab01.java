bdbaf025c06a77d6fc771f0a23ed759157021237
javaprofsmc <sbroad@saintmarys.edu>
2013-12-10 13:00:20 -0500

    Initial Skeleton

--- src/cp/lab/Lab01.java
+++ src/cp/lab/Lab01.java
@@ -7,78 +7,79 @@
 package cp.lab;
 
 /**
+ * CPSC 207: Computer Programming<br/>
+ * Lab 1: Getting to know NetBeans, Java and printing to the Java Console<br/>
+ * <br/>
  * In this lab, you will become familiar with NetBeans, the Java console, and 
  * printing to the Java console. You will also learn how to send me your
- * assignments.
+ * assignments.<br/>
  * 
- * CPSC 207: Computer Programming
- * Lab 1: Getting to know NetBeans, Java and printing to the Java Console
+ * <ol>
+ * <li> Open NetBeans.</li>
  * 
- * 1. Open NetBeans.
+ * <li> Create a NetBeans project. (File>New Project...).<ol type="a">
+ *      <li> Choose Java from the list on the left.</li>
+ *      <li> Choose Java Application on the right.  Click next.</li>
+ *      <li> Change the project name to Lab01.</li>
+ *      <li> Create your project in the My Documents-> NetBeansProjects folder.</li>
+ *      <li> Click finish.</li></ol></li>
  * 
- * 2. Create a NetBeans project. (File>New Project...).
- *      a. Choose Java from the list on the left.
- *      b. Choose Java Application on the right.  Click next.
- *      c. Change the project name to Lab01.
- *      d. Create your project in the My Documents-> NetBeansProjects folder.
- *      e. Click finish.
+ * <li> Wait a little bit. The program should create and open a file called 
+ * Lab01.java.</li>
  * 
- * 3. Wait a little bit. The program should create and open a file called 
- * Lab01.java.
+ * <li>4. Replace everything that is in your Lab01.java file with this file.</li>
  * 
- * 4. Replace everything that is in your Lab01.java file with this file.
+ * <li> Look to see if there are any problems. These would be underline with a red
+ * squiggle. Look especially at line 7 above.</li>
  * 
- * 5. Look to see if there are any problems. These would be underline with a red
- * squiggle. Look especially at line 7 above.
+ * <li> If you find such a problem, click on the appropriate line of code.<ol>
+ *      <li> NetBeans may show you a red light bulb in the margin.</li>
+ *      <li> Click the red light bulb. It presents you with options. In the case
+ *      of line 7, select "Change package declaration to [...] ..."</li>
+ *      <li> It changes the code automatically.</li></ol></li>
  * 
- * 6. If you find such a problem, click on the appropriate line of code.
- *      a. NetBeans may show you a red light bulb in the margin.
- *      b. Click the red light bulb. It presents you with options. In the case
- *      of line 7, select "Change package declaration to [...] ..."
- *      c. It changes the code automatically.
+ * <li> Find public static void main(String [] args). This is your "main method".
+ * The main method is where the main code for the program goes.</li>
  * 
- * 7. Find public static void main(String [] args). This is your "main method".
- * The main method is where the main code for the program goes.
+ * <li> Find and delete // TODO code application logic here. At the end of that 
+ * line, press the enter key to obtain a new line.</li>
  * 
- * 8. Find and delete // TODO code application logic here. At the end of that 
- * line, press the enter key to obtain a new line.
- * 
- * 9. Write 3 lines of code (use the println method) that print three separate 
+ * <li> Write 3 lines of code (use the println method) that print three separate 
  * personal goals for you in this class. You are welcome to refer to the 
- * syllabus to recall what sorts of learning goals to anticipate.
+ * syllabus to recall what sorts of learning goals to anticipate.</li>
  * 
- * 10. Add several comments (notes to yourself) in your code using //
+ * <li> Add several comments (notes to yourself) in your code using //</li>
  * 
- * 11. Build and run your project using the green "play" button on the NetBeans
- * toolbar.
+ * <li> Build and run your project using the green "play" button on the NetBeans
+ * toolbar.</li>
  * 
- * 12. Examine the text output of your program. It will appear in a text area 
+ * <li> Examine the text output of your program. It will appear in a text area 
  * below the code. This is called the Java Console. Does the program do what you
- * expected?
+ * expected?</li>
  * 
- * 13. Create documentation for your project. 
- *      a. Add a description of your program after the * on line 88. For
- *      example, you could type "This is our first Java lab. I’m almost finished."
- *      b. Select Run->Generate Javadoc (...) from the menu in NetBeans. This 
+ * <li> Create documentation for your project. <ol>
+ *      <li> Add a description of your program after the * on line 88. For
+ *      example, you could type "This is our first Java lab. I’m almost finished."</li>
+ *      <li> Select Run->Generate Javadoc (...) from the menu in NetBeans. This 
  *      will open a web browser window with your Javadoc in it. Find the lab
- *      description and the text that you typed.
+ *      description and the text that you typed.</li></ol></li>
  * 
- * 14. Prepare your Google Drive. (One time only)
- *      a. Login to <a href="https://drive.google.com">drive.google.com</a>.
- *      with your Saint Mary's email and password.
- *      b. If you have not done so, activate Google drive.
- *      c. Create a folder that uses your first initial, last 
+ * <li> Prepare your Google Drive. (One time only)<ol>
+ *      <li> Login to <a href="https://drive.google.com">drive.google.com</a>.
+ *      with your Saint Mary's email and password.</li>
+ *      <li> If you have not done so, activate Google drive.</li>
+ *      <li> Create a folder that uses your first initial, last 
  *      name and the course number. For example AJones207. Today and in the
- *      future, you will put your project in this folder so I can see it.
- *      d. Share this folder with me. This will ensure that I can see all of the
- *      work you send to me.
+ *      future, you will put your project in this folder so I can see it.</li>
+ *      <li> Share this folder with me. This will ensure that I can see all of the
+ *      work you send to me.</li></ol></li>
  * 
- * 15. Upload your project folder to Google Drive. Use the button that looks
- * like an upward arrow on top of a line.
- *      a. Select Folder...
- *      b. Find My Documents-> NetBeansProjects.
- *      c. Select the Lab01 folder.
- * 
+ * <li> Upload your project folder to Google Drive. Use the button that looks
+ * like an upward arrow on top of a line.<ol>
+ *      <li> Select Folder...</li>
+ *      <li> Find My Documents-> NetBeansProjects.</li>
+ *      <li> Select the Lab01 folder.</li></ol></li>
+ * </ol>
  * 
  * @author sbroad
  */

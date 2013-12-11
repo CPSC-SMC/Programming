d43136789c049e45d7eef463589fcbe01a574c88
Steven Broad <sebroad@yahoo.com>
2013-12-10 22:25:07 -0500

    Homework 2 prepared.

--- src/cp/hw/Homework3.java
+++ src/cp/hw/Homework3.java
@@ -6,10 +6,141 @@
 
 package cp.hw;
 
+import cp.HomeworkException;
+
 /**
- *
+ * <p>CPSC 207: Computer Programming<br/>
+ * Homework 3: Classes, methods, instances, objects.</p>
+ * 
+ * <p>Complete each of the methods in Homework3 below. Each method describes what
+ * it is supposed to do. Use the <code>return</code> statement to return the
+ * required information.</p>
+ * 
+ * <p>Create a project called Homework3. Copy this file over the contents of
+ * Homework3.java. The main method does not need to be modified. Most of your
+ * work will be done in the <code>class Student</code> declaration below.</p>
+ * 
+ * <p>Submit your project in the usual way. See Lab01 for details.</p>
+ * 
  * @author sbroad
  */
 public class Homework3 {
     
+    /**
+     * A test program for Homework 3 exercises. Do not modify.
+     * @param args command line arguments
+     */
+    public static void main(String [] args) {
+        
+        // create a new student object
+        Student s = new Student("Josie", 890101202);
+        
+        // make a note of grades the student has received
+        s.addCourse(3, 3); // B in a 3 credit class
+        s.addCourse(4, 3.333); // B+ in a 4 credit class
+        s.addCourse(3, 4); // A in a 3 credit course
+        s.addCourse(3, 2.333); // C+ in a 3 credit course
+        s.addCourse(4, 3.667); // A- in a 4 credit course
+        
+        // print information about the student
+        System.out.printf("%s has id %d.\n", s.getName(), s.getID());
+        System.out.printf("%s has %d credits.\n", s.getName(), s.getCredits());
+        System.out.printf("%s has %.4f quality points.\n", s.getName(), s.getQualityPoints());
+        System.out.printf("%s has %.2f GPA.\n", s.getName(), s.gpa());
+
+        // print the student
+        System.out.println(s);
+        
+    }
+}
+
+/**
+ * The Student class represents a student. Each student has a name, an ID, 
+ * a number of credits passed, and a number of quality points. Students can 
+ * take a class and calculate a gpa. You need to:
+ * 
+ * 1. Declare instance variables for name, id, credits and quality points.
+ * 2. Implement a constructor that accepts a name and id as parameters,
+ *  and initializes the credits and quality points to 0;
+ * 3. Implement accessor methods for each variable.
+ * 4. Implement a method called gpa() to compute the gpa for the student.
+ *          GPA = quality points / credits earned.
+ * 5. Implement a method called addCourse(int credits, double grade) to add 
+ *    a course to a student's "transcript." This method should accept a 
+ *    number of credits and a grade (a possibly fractional number from 0 to
+ *    4. This method adds to the student's credits and to the student's
+ *    quality points.
+ * 6. Implement the toString method, to make Student objects printable.
+ * 
+ */
+class Student {
+
+    // TODO: declare appropriate attributes i.e., instance variables
+    
+    /**
+     *
+     * @param name
+     * @param ID
+     */
+    public Student(String name, long ID) {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public long getID() {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getCredits() {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getQualityPoints() {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double gpa() {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @param credits
+     * @param grade
+     */
+    public void addCourse(int credits, double grade) {
+        throw new HomeworkException();
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        throw new HomeworkException();
+    }
 }

cdb69e882009636540afef71a55d07de64c328c7
javaprofsmc <sbroad@saintmarys.edu>
2014-01-23 10:55:27 -0500

    Changes to Homework 5 and 6

--- src/cp/hw/Homework06.java
+++ src/cp/hw/Homework06.java
@@ -10,9 +10,8 @@ package cp.hw;
  * <p>Homework 6: Declaring and Creating Arrays</p>
  * <p>CPSC 207: Computer Programming</p>
  * 
- * <p>Complete each of the methods in Homework06 below. Each method describes what
- * it is supposed to do. Use the <code>return</code> statement to return the
- * required information.</p>
+ * <p>Complete the main method in Homework06 below. Follow the instructions 
+ * in the main method.</p>
  * 
  * <p>Create a project called Homework06. Copy this file over the contents of
  * Homework06.java. The main method does not need to be modified. Remove the lines
@@ -23,5 +22,18 @@ package cp.hw;
  * @author sbroad
  */
 public class Homework06 {
-    
+    public static void main(String [] args) {
+        // Declare an array of ints containing 71, 37, 91, 23, and 53
+        // Print each of them
+        
+        // Add the Person class to your project from
+        // https://raw2.github.com/CPSC-SMC/Programming/201402/src/cp/lib/Person.java
+        // It will be a second .java file in your project.
+        // 
+        
+        // Declare and create space for an array of 100 Person objects.
+        
+        // Add the Homework06.txt file to your project from
+        // 
+    }
 }

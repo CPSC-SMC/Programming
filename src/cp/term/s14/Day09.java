9821ae93a6375d83cf82d07112c55b44c9359931
sbroad <sbroad@mad308-lp1.saintmarys.edu>
2014-02-17 13:02:58 -0500

    February 17

--- src/cp/term/s14/Day09.java
+++ src/cp/term/s14/Day09.java
@@ -26,20 +26,52 @@ public class Day09 {
             System.out.println("Let me go look in the back");
         }
         
+        if (shoeSize > 13) {
+            System.out.println("Too big.");
+        }
+        else if (shoeSize < 5) {
+            System.out.println("Too small");
+        }
+        else
+        {
+            System.out.println("Just right");
+        }
+            
         // Do you like walnuts in your brownies?
-        System.out.print("Do you like walnuts in your brownies? ");
+        System.out.print("Do you like walnuts in your brownies (Yes/No)? ");
         String walnuts = scan.nextLine();
         
         // if she has big feet and doesn't like walnuts, make fun of her
+        if (shoeSize > 13 && walnuts.toUpperCase().startsWith("NO")) {
+            System.out.println("Is that because you don't like to step on them with your big feet?");
+        }
         // if she has small feet and doesn't like walnuts, exclude her from the club
+        else if (shoeSize <= 13 && walnuts.equalsIgnoreCase("no")) {
+            System.out.println("I guess you won't be joining our club then.");
+        }
         // otherwise, ask her to join the club
-        
+        else {
+            System.out.println("Do you want to join our walnut brownie club?");
+        }
         
         // while loops: are we there yet?
         // while (_______) {
         //  ----->boolean expression
         //  do what is in the {}
         // }
+        String answer = "no";
+        boolean keepGoing = answer.equalsIgnoreCase("no");
+        while(keepGoing) {
+            System.out.print("Are we there yet? ");
+            answer = scan.nextLine();
+            keepGoing = answer.equalsIgnoreCase("no");
+        }
+        
+        int count = 0;
+        while (count < 10) {
+            System.out.println(count);
+            count += 2; // adds one to count
+        }
         
         // for loops
 

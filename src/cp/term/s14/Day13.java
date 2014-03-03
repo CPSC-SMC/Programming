3535b07cb0cc33e8342709a938ebb6c25adae053
javaprofsmc <sbroad@saintmarys.edu>
2014-03-03 11:56:40 -0500

    Prepping for Day 13

---
+++ src/cp/term/s14/Day13.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cp.term.s14;
+
+import edu.saintmarys.SimpleZip;
+
+/**
+ *
+ * @author sbroad
+ */
+public class Day13 {
+    public static void main(String []  args) {
+        
+        // Declare and create
+        SimpleZip [] simples = new SimpleZip[10];
+                
+        // When we create an array, it only saves 
+        // space for a certain number of
+        // objects. We still have to put them 
+        // into the array.
+        int index;  // position we are currently looking at
+        int count = 0; // number of positions filled
+                        // in the array
+
+        // INSERT: adding an item to an array
+        //      add each zip code to the simplezip array
+        
+        
+        
+        // Print the zip code
+        System.out.println("Below are the filled in positions");
+        for (index = 0; index < count; index++) {
+            System.out.println(simples[index]);
+        }
+        
+        
+        // FIND: Search for an item in an array
+
+        
+        // Loop until you find a match or run out of items
+
+        
+        // Did you find a match??
+
+        
+        
+        // FIND: An item in an array of objects, by searching for one attribute
+
+        
+        
+        // Loop until you find a match or run out of items
+
+        
+        
+        // Did you find a match??
+
+        
+        
+        // Print the final array
+
+    
+
+
+    }
+}
